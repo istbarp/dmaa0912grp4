53638cb9fe16976cd15e33f8779ca20827f59d11
Jackie <jackie.engberg@get2net.dk>
2014-02-25 21:30:24 +0100

    PathFinder script added

---
+++ Assets/AI/PathFinder.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PathFinder : MonoBehaviour
+{
+	public GameObject[] WayPoint;
+	public double Offset = 1;
+	private int CurrentWaypoint;
+
+	// Use this for initialization
+	void Start ()
+	{
+        CurrentWaypoint = 0;
+        if (Offset < 0.01)
+        {
+            Offset = 0.01;
+        }
+        if (Offset > 10)
+        {
+            Offset = 10;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		this.transform.position = Vector3.MoveTowards(this.transform.position, WayPoint[CurrentWaypoint].transform.position, Time.deltaTime);
+        this.transform.LookAt(WayPoint[CurrentWaypoint].transform.position);
+		if (Vector3.Distance(this.transform.position, WayPoint[CurrentWaypoint].transform.position) < Offset)
+		{
+			if (CurrentWaypoint == WayPoint.Length - 1)
+			{
+				Destroy(this.gameObject);
+				//TODO: Decrease lifes left by one - Need script
+			}
+			else
+			{
+				CurrentWaypoint++;
+			}
+		}
+	}
+}
