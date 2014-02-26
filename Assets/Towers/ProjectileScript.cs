82eaed0dea82388e093d92bb5fa9cb4da3eb5b3f
istbarp <unic_patrick@hotmail.com>
2014-02-26 02:01:33 +0100

    Towerscripts
    Some towerScripts added to assets

---
+++ Assets/Towers/ProjectileScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileScript : MonoBehaviour {
+
+	private double projectileSpeed = 5.0f;
+	private double projectileRange = 1.5f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.Translate (Vector3.forward * projectileSpeed);
+		double distance = projectileSpeed * Time.deltaTime;
+
+		if(distance >= projectileRange)
+		{
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Assets/Towers/TurretScript.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurretScript : MonoBehaviour {
+
+
+	public double reloadtime = 1f;
+
+	private Transform target = null;
+	private Quaternion rotation;
+	private double nextMoveTime;
+	private double nextFireTime;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (target != null) 
+		{
+			if(Time.time >= nextMoveTime)
+			{
+				CalculateAim(target.position);
+			}
+
+			if(Time.time >= 0.75f * Time.deltaTime)
+			{
+				fire ();
+			}
+		}
+	}
+
+	private void CalculateAim(Vector3 targetPosition)
+	{
+		Vector3 aimPoint = new Vector3 (targetPosition.x, targetPosition.y, targetPosition.z);
+		rotation = Quaternion.LookRotation (aimPoint);
+	}
+
+	private void fire()
+	{
+		Instantiate (ProjectileScript, transform.position, rotation);
+	}
+}
